50e007f4af61762bc420f659d46fffa1e623520a
rubel <rubel162765@gmail.com>
2026-06-23 18:48:46 +0600

    added statistic setion in owner dashboardpage

--- proxy.ts
+++ proxy.ts
@@ -7,13 +7,14 @@ export async function proxy(request: NextRequest) {
     const session = await auth.api.getSession({
       headers: request.headers,
     });
-    if (session?.user?.userRole === "tenant" && session?.user?.plan === "free") {
-      return NextResponse.redirect(new URL("/plan", request.url));
-    }
 
-    if (session?.user?.userRole === "owner" && session?.user?.plan === "free") {
-      return NextResponse.redirect(new URL("/plan", request.url));
-    }
+    // if (session?.user?.userRole === "tenant" && session?.user?.plan === "free") {
+    //   return NextResponse.redirect(new URL("/plan", request.url));
+    // }
+
+    // if (session?.user?.userRole === "owner" && session?.user?.plan === "free") {
+    //   return NextResponse.redirect(new URL("/plan", request.url));
+    // }
 
     if (!session) {
       return NextResponse.redirect(new URL("/auth/login", request.url));
